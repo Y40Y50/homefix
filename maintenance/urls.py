5e46361558f3f68fa86d8a98f9ab40e1026f2567
yahya <y40y50@gmail.com>
2026-06-09 18:04:08 +0100

    Create property form page

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('properties/', views.property_list, name='property_list'),
     path('contractors/', views.contractor_list, name='contractor_list'),
+    path('properties/create/', views.create_property, name='create_property'),
 ]
--- maintenance/views.py
+++ maintenance/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Property, Contractor
+from .forms import PropertyForm
 
 def home(request):
     return render(request, 'maintenance/home.html')
@@ -19,4 +20,19 @@ def contractor_list(request):
         request,
         'maintenance/contractor_list.html',
         {'contractors': contractors}
+    )
+
+# View to create a new property
+def create_property(request):
+
+    form = PropertyForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(
+        request,
+        'maintenance/create_property.html',
+        context
     )
