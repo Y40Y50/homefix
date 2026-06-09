c0afd433efa511a35f7e7559cdc77c650b59c5c4
yahya <y40y50@gmail.com>
2026-06-09 15:44:39 +0100

    Add MaintenanceJob model and relationships

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -1,7 +1,8 @@
 # imports Django's Admin Panel and the Property model
 from django.contrib import admin
-from .models import Property, Contractor
+from .models import Property, Contractor, MaintenanceJob
 
-# Show the Property and Contractor models inside the Admin Panel
+# Show the Property, Contractor, and MaintenanceJob models inside the Admin Panel
 admin.site.register(Property)
 admin.site.register(Contractor)
+admin.site.register(MaintenanceJob)
--- maintenance/models.py
+++ maintenance/models.py
@@ -48,4 +48,45 @@ class Contractor(models.Model):
 
     def __str__(self):
         return self.name
-    
+# Creating the Maintenance Job Table
+class MaintenanceJob(models.Model):
+
+    PRIORITY_CHOICES = [
+        ('high', 'High'),
+        ('medium', 'Medium'),
+        ('low', 'Low'),
+    ]
+
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ]
+
+    property = models.ForeignKey(
+        Property,
+        on_delete=models.CASCADE
+    )
+
+    contractor = models.ForeignKey(
+        Contractor,
+        on_delete=models.CASCADE
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
