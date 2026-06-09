64aaac0b1fe8ce502e8acc4faccd62508dbbaca8
yahya <y40y50@gmail.com>
2026-06-09 21:54:03 +0100

    Complete login, logout and registration

---
+++ accounts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
+
+def register(request):
+
+    if request.method == 'POST':
+
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect('home')
+
+    else:
+
+        form = RegisterForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(
+        request,
+        'accounts/register.html',
+        context
+    )
--- homefix/settings.py
+++ homefix/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'maintenance',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -111,6 +112,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
