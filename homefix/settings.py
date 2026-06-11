3aa7e3004f5f588c34682c22d047ea77a2fb8095
yahya <y40y50@gmail.com>
2026-06-11 11:08:07 +0100

    Add alowed hosts

--- homefix/settings.py
+++ homefix/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s5@8f0@=a57tbr+j78gf!%b_n!3zhob&)sx^n%mayexucbnuk@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
